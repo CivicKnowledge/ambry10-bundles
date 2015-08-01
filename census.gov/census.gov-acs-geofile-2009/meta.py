c53d8f0af8733b225b029c1f735f7607ea9f3e39
Eric Busboom <eric@clarinova.com>
2015-08-01 14:15:57 -0700

    Added all three geofiles for the year.

--- census.gov/census.gov-acs-geofile-2009/meta.py
+++ census.gov/census.gov-acs-geofile-2009/meta.py
@@ -4,33 +4,33 @@ import ambry.bundle
 class Bundle(ambry.bundle.Bundle):
     
     def mkschema(self):
+        """Create the 2009 geofile schema from the configuration in the upstream bundle. """
         from ambry.orm.file import File
    
         t = self.dataset.new_table('geoschema')
         st = self.dataset.new_source_table('geoschema')
         
-        for s in self.sources:
-            print s
-            p = self.library.partition(s.ref)
-            for row in p.stream(as_dict = True):
+        s = self.source('geofile_schema')
+ 
+        p = self.library.partition(s.ref)
+        for row in p.stream(as_dict = True):
 
-                if  row['year'] != 2009:
-                    continue
-                    
-                if row['name'].lower().strip() == 'blank':
-                    continue
-               
-                self.logger.info(row['name'])
+            if  row['year'] != 2009:
+                continue
                 
-                
-                t.add_column(row['name'].lower().strip(), 
-                        datatype = 'varchar',
-                        description = row['description'])
-                
-                st.add_column( source_header = row['name'].lower().strip(), position = row['seq'],
-                        datatype = str,
-                        start = row['start'], width = row['width'], 
-                        description = row['description'])
+            if row['name'].lower().strip() == 'blank':
+                continue
+           
+            self.logger.info(row['name'])
+            
+            t.add_column(row['name'].lower().strip(), 
+                    datatype = 'varchar',
+                    description = row['description'])
+            
+            st.add_column( source_header = row['name'].lower().strip(), position = row['seq'],
+                    datatype = str,
+                    start = row['start'], width = row['width'], 
+                    description = row['description'])
 
         self.commit() 
         
@@ -39,40 +39,92 @@ class Bundle(ambry.bundle.Bundle):
         self.do_sync(force='rtf')
         
     def add_sources(self):
-        
-        year = 2009
-        span = 1
-        source_name = 'dnlpage{}{}'.format(year,span)
-        source = self.source(source_name)
-    
-        
         from ambry.orm import DataSource, File
         from ambry.util import scrape_urls_from_web_page
+        
+        year = 2009
+
+        for span in [1,3]:
+            source_name = 'dnlpage{}{}'.format(year,span)
+            source = self.source(source_name)
                 
-        entries = scrape_urls_from_web_page(source.url)['sources']
+            entries = scrape_urls_from_web_page(source.url)['sources']
+            s = self.session
+            for k,v in entries.items():
+            
+                d = {
+                    'name': k.lower()+"_{}{}".format(year,span),
+                    'source_table_name': 'geoschema',
+                    'dest_table_name': 'geoschema',
+                    'filetype': 'fixed',
+                    'file': 'g2009.*\.txt',
+                    'encoding': 'latin1',
+                    'time': year, 
+                    'grain': span, 
+                    'url': v['url']
+                }
+            
+                ds = self._dataset.source_file(d['name'])
+                if ds:
+                    ds.update(**d)
+                else:
+                    ds = DataSource(**d)
+                    ds.d_vid = self.dataset.vid
+
+                s.merge(ds)
+            
+            s.commit()
+
+  
+    def add_5yr_sources(self):
+        from ambry.orm import DataSource, File
+        from ambry.util import scrape_urls_from_web_page
+        import os
+        
+        year = 2009
+        span = 5
+
+        source_name = 'dnlpage{}{}'.format(year,span)
+        source = self.source(source_name)
+
+        self.log("Loading from {}".format(source.url))
+
+        state_entries = scrape_urls_from_web_page(source.url)['links']
         s = self.session
-        for k,v in entries.items():
+        for state_name, parts in state_entries.items():
+            if state_name.endswith('/'):
+                state_name = state_name.replace('/','')
+                url = parts['url']
+                
+                for suffix in ('All_Geographies_Not_Tracts_Block_Groups', 'Tracts_Block_Groups_Only'):
+                    gurl = os.path.join(url, suffix)
+                    table_urls = scrape_urls_from_web_page(gurl)['sources']
+                    for k, v in table_urls.items():
+                        if k.startswith('g20095'):
+                            self.log('Found: {}'.format(k))
+                            d = {
+                                'name': k,
+                                'source_table_name': 'geoschema',
+                                'dest_table_name': 'geoschema',
+                                'filetype': 'fixed',
+                                'file': 'g2009.*\.txt',
+                                'encoding': 'latin1',
+                                'time': year, 
+                                'grain': span, 
+                                'url': v['url']
+                            }
             
-            d = {
-                'name': k.lower()+"_{}{}".format(year,span),
-                'source_table_name': 'geoschema',
-                'dest_table_name': 'geoschema',
-                'filetype': 'fixed',
-                'file': 'g2009.*\.txt',
-                'encoding': 'latin1',
-                'url': v['url']
-            }
-            
-            ds = self._dataset.source_file(d['name'])
-            if ds:
-                ds.update(**d)
-            else:
-                ds = DataSource(**d)
-                ds.d_vid = self.dataset.vid
+                            ds = self._dataset.source_file(d['name'])
+                            if ds:
+                                ds.update(**d)
+                            else:
+                                ds = DataSource(**d)
+                                ds.d_vid = self.dataset.vid
 
-            s.merge(ds)
-            
-        s.commit()
-
+                            s.merge(ds)
+                            
+                            
+                
+        
         
             
