dab070cc005ba2d483cc53b5e633f8286944341a
Eric Busboom <eric@clarinova.com>
2015-10-11 21:42:43 -0700

    .

--- civicknowledge.com/names-census/bundle.py
+++ civicknowledge.com/names-census/bundle.py
@@ -9,10 +9,6 @@ class Bundle(ambry.bundle.Bundle):
         from operator import itemgetter
         
         p =  self.partition(table="counties")
-        ig = itemgetter(*p.headers)
-        
-        #with p.reader as r:   
-        #    print { row['geoid']:row['name'] for row in r}
-         
+
         x = dict( [row.row for row  in p.select( headers=('gvid', 'mid_name')) ])
         print x
