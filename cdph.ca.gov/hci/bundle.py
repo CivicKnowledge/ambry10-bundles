79c977c39542e6078312257c0902852d35e14f97
Eric Busboom <eric@clarinova.com>
2015-12-08 15:51:36 -0800

    .

--- cdph.ca.gov/hci/bundle.py
+++ cdph.ca.gov/hci/bundle.py
@@ -1,15 +1,54 @@
 # -*- coding: utf-8 -*-
 import ambry.bundle
 
-
 class Bundle(ambry.bundle.Bundle):
     
-    RSE_MAX = 100 # Maximum Relative std error
     
-    def catch_dbz(self, v):
+    def catch_dbz(self, row,  v):
         """On row 96767 of the Alameda Neighbohoor CHange file there is a
         very large value that looks like a divide-by-nearly-zero error"""
 
-        return min(v, self.RSE_MAX)
+        if row.difference == 0:
+            return None
+        else:
+            return v
         
+        
+    def version_date(self, v):
+        """Deal with wacky version dates, like: 14APR13:10:31:45"""
+        
+        from dateutil import parser
+        from xlrd.xldate import  xldate_as_datetime
+          
+        try:
+            v =  parser.parse("{}-{}-{}".format(v[0:2],v[2:5],v[5:7])).date()
+        except:
+            v = xldate_as_datetime(v, 0).date()
+            
+        return v  
+        
+    def do_row(self, row, v, accumulator):
+        
+        # These value are mostly the same for every row, I think. 
+        accumulator[row.ind_id] = row.ind_definition
        
+        return v
+        
+    def do_final(self, pl):
+         
+        from ambry.etl import CastColumns
+        
+        caster = pl[CastColumns]
+        
+        table = caster.source.dest_table
+        
+        table.description = ','.join( '{}: {}'.format(k, v) 
+                     for k, v in caster.accumulator.items())
+        
+        
+        
+        
+    def edit_pipeline(self, pl):
+        
+        pl.final.append(self.do_final)
+        return pl
--- cdph.ca.gov/hci/documentation.md
+++ cdph.ca.gov/hci/documentation.md
@@ -3,7 +3,5 @@
 Caveats
 =======
 
-In the ``Neighborhood Change`` files, the Relative Standard Error (rse)
-column is often computed for values that are ver close to zero, so the 
-RSE is very large. In other files in this dataset, the rse value is capped
-at 100, so values in the Neighborhood Change file is also capped at 100. 
+As of Dec 1, 2015, In the ``Neighborhood Change`` files, the Relative Standard Error (rse) column is often computed for values that are ver close to zero, so the RSE is very large. In other files in this dataset, the rse value is capped
+at 100. As per Dulce Bustamante-Zamora at CDPH, these values should be blank, (NULL) so this correction is made for rows where the difference is 0. 
