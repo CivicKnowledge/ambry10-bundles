d63e7696eb9eb2e08e411c97f23f2524894b8741
Eric Busboom <eric@clarinova.com>
2015-12-09 13:55:03 -0800

    .

--- cdph.ca.gov/hci/bundle.py
+++ cdph.ca.gov/hci/bundle.py
@@ -1,18 +1,45 @@
 # -*- coding: utf-8 -*-
 import ambry.bundle
 
+
+
 class Bundle(ambry.bundle.Bundle):
     
+    def edit_pipeline(self, pl):
+        
+        from ambry.etl import SelectPartition
+        
+    
+        pl.final = [self.edit_descriptions]
+        pl.select_partition = [SelectPartition(
+        'dict(table=source.dest_table.name,'
+        'segment=source.sequence_id,'
+        'grain=row.geotype.lower())'
+        )]
+        return pl
+  
+        
+    def na_is_none(self, v):
+        
+        if v == 'NA':
+            return None
+        else:
+            return v
     
     def catch_dbz(self, row,  v):
         """On row 96767 of the Alameda Neighbohoor CHange file there is a
         very large value that looks like a divide-by-nearly-zero error"""
 
-        if row.difference == 0:
+        try:
+            if row.difference == 0:
+                return None
+        except KeyError:
+            pass
+            
+        if v == 'NA':
             return None
         else:
             return v
-        
         
     def version_date(self, v):
         """Deal with wacky version dates, like: 14APR13:10:31:45"""
@@ -27,14 +54,16 @@ class Bundle(ambry.bundle.Bundle):
             
         return v  
         
-    def do_row(self, row, v, accumulator):
+    def extract_desc(self, v, row, accumulator):
+        """Extract the table description from the ind_definition field"""
         
         # These value are mostly the same for every row, I think. 
         accumulator[row.ind_id] = row.ind_definition
-       
         return v
         
-    def do_final(self, pl):
+    def edit_descriptions(self, pl):
+        """Extract the values added to the accumulator by extract_desc and add 
+        them to the table description"""
          
         from ambry.etl import CastColumns
         
@@ -42,13 +71,56 @@ class Bundle(ambry.bundle.Bundle):
         
         table = caster.source.dest_table
         
-        table.description = ','.join( '{}: {}'.format(k, v) 
+        table.description = ','.join( u'HCI Indicator {}: {}'.format(k, v) 
                      for k, v in caster.accumulator.items())
+                     
+    def extract_geoid(self, v, row):
+        
+        from geoid.census import Place, County, State, Cosub, Tract, Zcta
+        from geoid.civick import GVid
+        
+        CA_STATE = 6
+        
+        if row.geotype == 'PL':
+            r = Place(CA_STATE, int(row.geotypevalue)).convert(GVid)
+        elif row.geotype == 'CO':
+            gt = row.geotypevalue
+            assert int(gt[0:2]) == CA_STATE
+            r = County(CA_STATE, int(gt[2:])).convert(GVid)
+        elif row.geotype == 'CA':
+            r = State(CA_STATE).convert(GVid)
+        elif row.geotype == 'CD':
+            r = Cosub.parse(row.geotypevalue).convert(GVid)
+        elif row.geotype == 'CT':
+            try:
+                r = Tract.parse(row.geotypevalue).convert(GVid)
+            except ValueError:
+                r = Tract.parse('06'+row.geotypevalue).convert(GVid)
+        elif row.geotype == 'ZC':
+            r = Zcta.parse(row.geotypevalue).convert(GVid)
+        elif row.geotype == 'NA': # Sub-state region, not a census area
+            r = None
+        elif row.geotype == 'RE': # Sub-state region, not a census area
+            r = None
+        else:
+            self.error("Unknown geotype {} in row {}".format(row.geotype, row))
+            r = None
+    
+        return r
+        
+    def meta_add_gvid(self):
+        """A meta phase routine to add the gvid columns to every table"""
+        
+        for t in self.tables:
+            t.add_column('gvid',datatype='census.GVid', 
+                description='GVid version of the geotype and geotypeval',
+                transform='^extract_geoid')
+                
+        self.commit()
         
         
         
         
-    def edit_pipeline(self, pl):
         
-        pl.final.append(self.do_final)
-        return pl
+                     
+                     
--- cdph.ca.gov/hci/documentation.md
+++ cdph.ca.gov/hci/documentation.md
@@ -1,7 +1,27 @@
 
 
+Region codes
+============
+
+The `geotype` field has a code the describes the type of geography used in 
+aggregating each row. These geographies are generally Census geographies, 
+and are specified with Census geoids, in the `geotypevalue` field. The
+`geotype` codes are:
+
+- `CA` The whole state of California
+- `CO` A county
+- `CD` A county subdivision
+- `PL` A Census Designated Place
+- `RE` A Sub-state region
+- `ZC` ZCTA, the Census version of a ZIP code area.
+
+
 Caveats
 =======
 
 As of Dec 1, 2015, In the ``Neighborhood Change`` files, the Relative Standard Error (rse) column is often computed for values that are ver close to zero, so the RSE is very large. In other files in this dataset, the rse value is capped
 at 100. As per Dulce Bustamante-Zamora at CDPH, these values should be blank, (NULL) so this correction is made for rows where the difference is 0. 
+
+Two fields have been removed from the tables, 'ind_id', the ID number for the indicator in the table, and 'ind_definition.' These values are the same for every row in a table, so they have been moved to the table description. 
+
+The `reportyear` field can be either a single integer year, or it may be a range of years, which is represented as a string. 
