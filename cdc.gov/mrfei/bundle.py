2c2f9863460744c59e9a28be9e68f7bf56d4d446
Eric Busboom <eric@clarinova.com>
2016-01-27 13:57:34 -0800

    Added geoid, state, county names

--- cdc.gov/mrfei/bundle.py
+++ cdc.gov/mrfei/bundle.py
@@ -3,5 +3,12 @@ import ambry.bundle
 
 
 class Bundle(ambry.bundle.Bundle):
-    pass
 
+    def parse_tiger_tract(self, v):
+        from geoid.census import Tract
+
+        if isinstance(v, (float, int)):
+            v = '{:0>11}'.format(int(v))
+        
+
+        return Tract.parse(v)
